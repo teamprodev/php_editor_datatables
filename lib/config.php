8aa086ac10e2b4228bf6a0d6b3d8e5fccd24ac98
xeroctation <umidakosimova.uk@gmail.com>
2022-12-15 14:48:10 +0500

    redirect fix

--- lib/config.php
+++ lib/config.php
@@ -10,11 +10,11 @@ ini_set('display_errors', '1');
  */
 $sql_details = array(
 	"type" => "Mysql",     // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => "root",          // Database user name
-	"pass" => "",          // Database password
+	"user" => "mysql",          // Database user name
+	"pass" => "mysql",          // Database password
 	"host" => "localhost",   // Database host
 	"port" => "3306",          // Database connection port (can be left empty for default)
-	"db"   => "laravel_editor",          // Database name
+	"db"   => "php_editor",          // Database name
 	"dsn"  => "utf8mb4",          // PHP DSN extra information. Set as `charset=utf8mb4` if you are using MySQL
 	"pdoAttr" => array()   // PHP PDO attributes array. See the PHP documentation for all options
 );
